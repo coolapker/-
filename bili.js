f2b2e33416ad4b4667a512f9e702a9c3c032d52f
mojospy <76961348+coolapker@users.noreply.github.com>
2023-07-03 11:16:04 +0800

    Update bili.js

--- bili.js
+++ bili.js
@@ -1,3 +1,14 @@
+[rewrite_local]
+  
+# ～ Bilibili大会员（2022-12-05）@ddgksf2013
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header BilibiliProCrack.js
+
+
+[mitm] 
+
+hostname=app.bilibili。com, grpc.biliapi。net,*。biliapi。net,app.bilibili。com,api.bilibili。com,api.live。bilibili。com,api.vc。bilibili。com,dataflow.biliapi。com,124.239。240.*,101.89。57.66, 218.94。210.66,240e:b1:9801:206:11:0:0:*
+
+
 var modifiedHeaders = $request['headers'];
 modifiedHeaders['Cookie'] = '_uuid=FD40FA45-78E5-B4BA-0E16-DDC66FEBA9A503478infoc; buvid3=31C0CF16-F604-49A7-A4A6-97AB6E70900236715infoc; buvid4=B3CFD40A-DA86-7AD0-ABEE-CF86C054839E03649-123070308-bK5ysmawZ6WuKUM2vI2CLg%3D%3D; Buvid=YC404ACC89F3A7474D9BA624A40C96B1D3C6; sid=ourmj9jh; SESSDATA=2be9f337%2C1703897446%2C9a69c072PSpJob_MzO-bj0WL2QFDnIN_kFch0h7Pm5eHNIQ30CQc6eAW8M7CfIx5Z0VCpGKng8qHggAAFAA; bili_jct=2a1269d3d77809b0298d0257fe417d05; DedeUserID=291204920; DedeUserID__ckMd5=9579e491512bd237';
 modifiedHeaders['x-bili-device-bin'] = 'CAEQyJmMIxokWUM0MDRBQ0M4OUYzQTc0NzREOUJBNjI0QTQwQzk2QjFEM0M2IgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoRaVBob25lIDE0IFBybyBNYXhSBjE2LjUuMWoGNy4zNi4wckBBRkM2NkMyQzc5NjhDQzRBOEFCMDI1NjlFNjk1NDJGQzIwMjMwNjEyMTQ1MzMzOEZGRTUzNENERTU2ODJEQjM3eJDfodeJMQ==';
