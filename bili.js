07da21a06762f006537c4566be9cbfad30aee8c4
mojospy <76961348+coolapker@users.noreply.github.com>
2023-07-03 11:00:50 +0800

    Update bili.js

--- bili.js
+++ bili.js
@@ -1,16 +1,8 @@
-
-
-
-[rewrite_local]
-  
+[rewrite_local]  
 # ～ Bilibili大会员（2022-12-05）@ddgksf2013
 ^http[s]?:\/\/((app|api)\。(\w{2,15})?\。(com|cn))。*player\。(v3|v2|v1)。Play(URL|View)。*$ url script-request-header BilibiliProCrack。js
-
-
 [mitm] 
-
 hostname=app。bilibili。com, grpc。biliapi。net,*。biliapi。net,app。bilibili。com,api。bilibili。com,api。live。bilibili。com,api。vc。bilibili。com,dataflow。biliapi。com,124。239。240。*,101。89。57。66, 218。94。210。66,240e:b1:9801:206:11:0:0:*
-
 
  
 
